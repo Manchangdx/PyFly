f8e6155f9c117ba9233add46f4a29f0a9d394d1b
Manchangdx <1195581533@qq.com>
2019-12-13 17:08:08 +0800

    完成实验 10 的修改

--- PyFly/fly_bbs/extensions.py
+++ PyFly/fly_bbs/extensions.py
@@ -1,9 +1,11 @@
 from flask_pymongo import PyMongo
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES, ALL
+from flask_admin import Admin
 from bson import ObjectId
 
 from .models import User
+from .admin import admin_view
 
 
 mongo = PyMongo()
@@ -27,6 +29,8 @@ def user_load(user_id):
 # 实例化图片上传对象，extensions 参数代表允许扩展名
 upload_photos = UploadSet(extensions=ALL)
 
+admin = Admin(name='PyFly 后台管理系统')
+
 
 def init_extensions(app):
     # 初始化 app 的时候，会调用 app.config 的 MONGO_URI 属性
@@ -40,3 +44,22 @@ def init_extensions(app):
     login_manager.init_app(app)
     # 获取配置信息并存储在 app 上
     configure_uploads(app, upload_photos)
+    admin.init_app(app)
+    with app.app_context():
+        admin.add_view(admin_view.OptionsModelView(mongo.db['options'], 
+                '系统设置'))
+        admin.add_view(admin_view.UsersModelView(mongo.db['users'], '用户管理'))
+        admin.add_view(admin_view.CatalogsModelView(mongo.db['catalogs'], 
+                '栏目管理', category='内容管理'))
+        admin.add_view(admin_view.PostsModelView(mongo.db['posts'], 
+                '帖子管理', category='内容管理'))
+        admin.add_view(admin_view.PassagewaysModelView(mongo.db['passageways'], 
+                '温馨通道', category='推广管理'))
+        admin.add_view(admin_view.FriendLinksModelView(mongo.db['friend_links'], 
+                '友链管理', category='推广管理'))
+        admin.add_view(admin_view.PagesModelView(mongo.db['pages'], '页面管理', 
+                category='推广管理'))
+        admin.add_view(admin_view.FooterLinksModelView(mongo.db['footer_links'], 
+                '底部链接', category='推广管理'))
+        admin.add_view(admin_view.AdsModelView(mongo.db['ads'], '广告管理', 
+                category='推广管理'))
